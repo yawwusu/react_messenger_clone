ebfc6b2ddb04c86f50812211f0ef409fbf2ba687
yawwusu <yawwusugh@gmail.com>
2021-04-22 12:17:43 +0200

    Initial commit - basic input functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Button from '@material-ui/core/Button'
+import { FormControl, Input, InputLabel } from '@material-ui/core';
+import Message from './Message';
 
 function App() {
+  const [input, setInput] = React.useState('');
+  const [messages, setMessages] = React.useState([]);
+
+  const handleChange = function(event) {
+    setInput(event.target.value)
+  }
+
+  const sendMessage = function(event) {
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Facebook Messenger Clone</h1>
       </header>
+      <main className="App-body">
+        <form>
+          <FormControl>
+            <InputLabel htmlFor="message">Enter a message</InputLabel>
+            <Input value={input} onChange={handleChange} id="message" aria-describedby="message-helper-text" />
+          </FormControl>
+          <Button disabled={!input} variant='contained' color='primary' type='submit' onClick={sendMessage}>Send Message</Button>
+        </form>
+
+        {
+          messages.map(message => (
+            <Message message={message} />
+          ))
+        }
+      </main>
     </div>
   );
 }
---
+++ src/Message.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Message({message}) {
+
+    return (
+        <div>
+          <h3>{message}</h3>
+        </div>
+    )
+}
+
+export default Message
