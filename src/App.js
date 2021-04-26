5071a193b7fc7cd3ff4debd99ef0e58ebc2c9770
yawwusu <yawwusugh@gmail.com>
2021-04-26 08:56:34 +0200

    Corrected styling and add flip move effect

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import { FormControl, Input, InputLabel } from '@material-ui/core';
 import Message from './Message';
 import db from './firebase'
 import firebase from 'firebase'
+import FlipMove from 'react-flip-move'
 
 function App() {
   const [input, setInput] = React.useState('');
   const [messages, setMessages] = React.useState([]);
-  const [username, setUserName] = React.useState('Yaw');
+  const [username, setUserName] = React.useState('');
 
   React.useEffect(() => {
     db.collection('messages')
       .orderBy('timestamp', 'desc')
       .onSnapshot(snapshot => {
         // console.log('data', snapshot.docs.map(doc => doc.data()))
-        setMessages(snapshot.docs.map(doc => doc.data()))
+        setMessages(snapshot.docs.map(doc => ({'id': doc.id, 'data': doc.data()})))
       })
   }, [])
 
@@ -57,11 +58,13 @@ function App() {
           <Button disabled={!input} variant='contained' color='primary' type='submit' onClick={sendMessage}>Send Message</Button>
         </form>
 
-        {
-          messages.map(message => (
-            <Message message={message} username={username} />
-          ))
-        }
+        <FlipMove>
+          {
+            messages.map(message => (
+              <Message key={message.id} message={message.data} username={username} />
+            ))
+          }
+        </FlipMove>
       </main>
     </div>
   );
--- src/Message.js
+++ src/Message.js
@@ -2,12 +2,12 @@ import { Card, CardContent, Typography } from '@material-ui/core'
 import React from 'react'
 import './Message.css';
 
-function Message({message, username}) {
+const Message = React.forwardRef(({message, username}, ref) => {
     const isUser = username === message.username;
     // console.log('isUser', isUser)
-    
+
     return (
-        <div className={`message__card ${isUser && 'message__card__user'}`}>
+        <div key={message.id} ref={ref} className={`message__card ${isUser && 'message__card__user'}`}>
             <Card className={isUser ? 'message__user' : 'message__guest'}>
                 <CardContent>
                     <Typography
@@ -20,6 +20,6 @@ function Message({message, username}) {
             </Card>
         </div>
     )
-}
+})
 
 export default Message
